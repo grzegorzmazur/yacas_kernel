1512d42de71cc9cc9fc3f4899c9471c2f88ede8e
Grzegorz Mazur <teoretyk@gmail.com>
2015-12-30 12:02:00 +0100

    fix program name in the license info

--- src/hmac_sha256.hpp
+++ src/hmac_sha256.hpp
@@ -1,11 +1,11 @@
 /*
  * This file is part of yacas_kernel.
- * Yacas is free software: you can redistribute it and/or modify
+ * Yacas_kernel is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesset General Public License as
  * published by the Free Software Foundation, either version 2.1
  * of the License, or (at your option) any later version.
  *
- * Yacas is distributed in the hope that it will be useful,
+ * Yacas_kernel is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
